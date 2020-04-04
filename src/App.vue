c465e970153e9a0524e505e0c863637c20962323
wangwenzhang <wangwenzhang@uniqueway.com>
2020-04-04 21:40:13 +0800

    add page

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,72 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="box-container">
+      <div class="banner"><repo/> </div>
+      <div class="content">
+        <hoc-el-affix>
+          <div class="box-1"></div>
+        </hoc-el-affix>
+        <div class="long-list">
+          <div
+            v-for="(item, index) in 100"
+            :key="index"
+            class="long"
+          >
+          {{ index === 99 ? '到底了' : `占位符${index + 1}` }}
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import repo from '@/components/repo'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    repo
   }
 }
 </script>
 
 <style>
+* {
+  padding: 0;
+  margin: 0;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
+
+<style lang="scss" scoped>
+.box-container {
+  .banner {
+    padding: 30px 0;
+    background-color: #eee;
+  }
+  .content {
+    position: relative;
+    width: 300px;
+    margin: 0 auto;
+    .box-1 {
+      width: 300px;
+      height: 50px;
+      border: 1px solid #000;
+    }
+    .long-list {
+      text-align: center;
+      .long {
+        height: 50px;
+        font-size: 16px;
+      }
+    }
+  }
+}
+</style>
+
--- src/main.js
+++ src/main.js
@@ -1,7 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
+import HocElementAffix from '@hoc-element/affix'
+
 
 Vue.config.productionTip = false
+
+Vue.use(HocElementAffix)
 
 new Vue({
   render: h => h(App),
