717c3a205a5619dac9a760bb8d771b9b56f7fea6
pdsuwwz <pdsu.wwz@foxmail.com>
2020-11-12 10:11:06 +0800

    Update hoc element table

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <TableList/>
+    <TableList />
   </div>
 </template>
 
@@ -16,11 +16,14 @@ export default {
 </script>
 
 <style>
+* {
+  margin: 0;
+  padding: 0;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
 }
 </style>
